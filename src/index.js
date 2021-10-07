2d65870772e1e430a21ce17359a442c5b12bb282
PavelLoboda <pavel.loboda@gmail.com>
2021-10-07 14:22:09 +0300

    Revert "upload source for mentor check"
    This reverts commit 403bc8779584a867c7a20b41ac842a2883e32c2d.

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './sass/main.scss';
-import './js/tools-bar.js';
-import './js/food-cards';
--- src/js/food-cards.js
+++
@@ -1,8 +0,0 @@
-import dataFoods from '../data/data.json';
-import createFoodCardTempl from '../templates/food-cards.hbs';
-
-////// RUN //////
-
-const foodCardsMarkup = createFoodCardTempl(dataFoods);
-const foodCardsContainerRef = document.querySelector('ul.js-menu');
-foodCardsContainerRef.insertAdjacentHTML('beforeend', foodCardsMarkup);
